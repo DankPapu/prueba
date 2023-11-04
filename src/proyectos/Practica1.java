3702c9ad1b6ab163400cf79abd50feac434c76ee
DANKPAPU <DANKPAPU@DESKTOP-KSQDHP7>
2023-11-04 13:22:02 -0500

    Updated

---
+++ src/proyectos/Practica1.java
@@ -0,0 +1,9 @@
+/*Javier ALejandro Torres Martinez ING.Tics Practica No.1 /Mensaje en pantalla*/
+package proyectos;
+
+public class Practica1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Este es un mensaje en pantalla");
+    }
+}
